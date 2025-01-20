539bcff92628f16b2a24d81528beb44f20dc425f
JackieAisa <aisakhunova@gmail.com>
2025-01-20 23:14:00 +0000

    update

--- Homework3/EC2.tf
+++ Homework3/EC2.tf
@@ -27,8 +27,7 @@ resource "aws_instance" "web" {
 
    ami = data.aws_ami.amazon_linux_2.id
    instance_type = "t2.micro"
-   availability_zone = "us-east-1a"
-   subnet_id = "subnet-0007ec70aa8145f82"
+   availability_zone = "us-east-1a"  # if more then one subnet in an AZ then subnet_id = "subnet-0007ec70aa8145f82"
    key_name = aws_key_pair.key.key_name
    vpc_security_group_ids = [aws_security_group.allow_tls.id]
    user_data = file("apache.sh")
@@ -43,7 +42,6 @@ resource "aws_instance" "web2" {
    ami = data.aws_ami.amazon_linux_2.id
    instance_type = "t2.micro"
    availability_zone = "us-east-1b"
-   subnet_id = "subnet-0b2c42bbebdb609c0"
    key_name = aws_key_pair.key.key_name
    vpc_security_group_ids = [aws_security_group.allow_tls.id]
    user_data = file("apache.sh")
@@ -58,7 +56,6 @@ resource "aws_instance" "web3" {
    ami = data.aws_ami.amazon_linux_2.id
    instance_type = "t2.micro"
    availability_zone = "us-east-1c"
-   subnet_id = "subnet-0cc2c4c9af64ea108"
    key_name = aws_key_pair.key.key_name
    vpc_security_group_ids = [aws_security_group.allow_tls.id]
    user_data = file("apache.sh")
